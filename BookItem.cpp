f5af6ffbef5cff2ce2fa9216b73bece9fd10ff1e
Tazeek <tazeek.rakib@gmail.com>
2015-01-03 01:32:05 -0800

    Create BookItem.cpp

---
+++ BookItem.cpp
@@ -0,0 +1,62 @@
+#ifndef BOOKITEM_CPP
+#define BOOKITEM_CPP
+#include "node.hpp"
+#include "BookItem.hpp"
+#include "XStack.hpp"
+#include "XStack.cpp"
+#include <iostream>
+#include <string>
+using namespace std;
+
+int BookItem::getSerialNum()
+{
+    return serialNum;
+}
+
+int BookItem::getYearPublished()
+{
+    return yearPublished;
+}
+
+int BookItem::getCopiesSold()
+{
+    return copiesSold;
+}
+
+string BookItem::getTitle()
+{
+    return title;
+}
+
+string BookItem::getAuthor()
+{
+    return author;
+}
+
+ostream& operator<<(ostream& os, const BookItem& s)
+{
+    os << "<" << s.serialNum << "> " << s.title << " : "
+       << s.author << " (" << s.yearPublished << ") - "
+       << s.copiesSold << " copies sold";
+
+    return os;
+}
+
+bool operator< (BookItem& b1, BookItem& b2)
+{
+    return b1.copiesSold < b2.copiesSold;
+}
+
+bool operator> (BookItem& b1, BookItem& b2)
+{
+    if (b1.yearPublished == b2.yearPublished)
+    {
+        return b1.title < b2.title;
+    }
+    else
+    {
+        return b1.yearPublished > b2.yearPublished;
+    }
+}
+
+#endif // BOOKITEM_CPP
